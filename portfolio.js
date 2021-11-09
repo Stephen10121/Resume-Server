ab78c2ed301d4c6a4011ca2e336aea1852ce9446
Stephen Gruzin <stephengrzn@gmail.com>
2021-11-08 16:02:24 -0800

    Working on dynamic rounded progress bars

--- portfolio.js
+++ portfolio.js
@@ -1,59 +1,23 @@
-var headerTabs = document.getElementById("header-tabs");
-var headerBigTab = document.getElementById("bigtab");
-var header = document.getElementById("header");
-var tabOpen = false;
-const tabSmall = document.getElementById("small-tab-slide");
-var tabButton = document.getElementById("showTabButton");
+const textPerc = document.getElementById("perc");
+const roundPerc = document.getElementById("progress-circle");
 
-function smallScreen() {
-    headerTabs.style.display = "block";
-    headerBigTab.style.display = "none";
-    header.style.gridTemplateColumns = "80% 20%";
+function map_range(value) {
+    return 189 + (0 - 189) * (value - 0) / (100 - 0);
 }
 
-function notSmallScreen() {
-    headerTabs.style.display = "none";
-    headerBigTab.style.display = "block";
-    header.style.gridTemplateColumns = "60% 40%";
-    closeTab();
+function setPerc(percentage) {
+    let perc = 1;
+    let jeff = setInterval(()=> {
+        if (perc==percentage) {
+            clearInterval(jeff);
+        }
+        textPerc.innerHTML = `${perc}%`;
+        roundPerc.style.strokeDashoffset = map_range(perc);
+        perc++;
+    }, 10);
 }
 
-if (document.body.clientWidth <= 630) {
-    smallScreen();
-} else {
-    notSmallScreen();
-}
+setPerc(50);
 
-window.addEventListener('resize', function(event) {
-    if (document.body.clientWidth <= 630) {
-        smallScreen();
-    } else {
-        notSmallScreen();
-    }
-}, true);
-
-function openTab() {
-    tabOpen = true;
-    tabButton.style.transform = 'rotate(90deg)';
-    tabSmall.style.display = "block";
-    setTimeout(() => {
-        tabSmall.style.top = "70px";
-    }, 80);
-}
-
-function closeTab() {
-    tabOpen = false;
-    tabButton.style.transform = 'rotate(0deg)';
-    tabSmall.style.top = "-100px";
-    setTimeout(() => {
-        tabSmall.style.display = "none";
-    }, 100);
-}
-
-function showTabs() {
-    if (tabOpen == false){
-        openTab();
-    } else {
-        closeTab();
-    }
-}
+//189 0
+//0 100
